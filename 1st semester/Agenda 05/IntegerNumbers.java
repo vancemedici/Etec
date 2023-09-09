44a2b293422a8190c189b62a52de29c8f19a2baf
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-08 21:30:55 -0300

    Update IntegerNumbers.java

--- 1st semester/Agenda 05/IntegerNumbers.java
+++ 1st semester/Agenda 05/IntegerNumbers.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package integernumbers;
 
 import javax.swing.JOptionPane;
-
-/**
- *
- * @author Brian
- */
 public class IntegerNumbers {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int num1, num2, plus, times;
         num1 = Integer.parseInt(JOptionPane.showInputDialog("Insira o primeiro número inteiro"));
