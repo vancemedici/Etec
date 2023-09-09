159e8c69a6e5a5f3bcd5404879f471f195497609
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-08 23:14:37 -0300

    Update CovidBrasil.java

--- 1st semester/Agenda 04/CovidBrasil.java
+++ 1st semester/Agenda 04/CovidBrasil.java
@@ -1,22 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package covidbrasil;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-/**
- *
- * @author Brian
- */
 public class CovidBrasil {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         DecimalFormat fmt = new DecimalFormat("##,###,###");
