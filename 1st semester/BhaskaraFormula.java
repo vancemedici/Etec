99a45cd6b9e98ce6874a3bfd2e42043829428944
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-09 00:17:12 -0300

    Update BhaskaraFormula.java

--- 1st semester/BhaskaraFormula.java
+++ 1st semester/BhaskaraFormula.java
@@ -1,21 +1,23 @@
 import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
         double a = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor de a"));
         double b = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor de b"));
         double c = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor de c"));
+        
         double delta = Math.pow(b, 2) - 4 * a * c;
+        JOptionPane.showMessageDialog(null, "O valor de delta é " + delta);
 
-        if (delta < 0){
+        if (delta < 0) {
             JOptionPane.showMessageDialog(null, "Não existem raízes reais");
         } else if (delta == 0) {
             double x = -b / (2 * a);
-            JOptionPane.showMessageDialog(null, "Só há uma raiz real, cujo valor é "+x);
-        } else{
+            JOptionPane.showMessageDialog(null, "Só há uma raiz real, cujo valor é " + x);
+        } else {
             double x1 = (-b + Math.sqrt(delta)) / (2 * a);
             double x2 = (-b - Math.sqrt(delta)) / (2 * a);
-            JOptionPane.showMessageDialog(null, "Os valores das raízes reais são iguais à "+x1+" e à "+x2);
+            JOptionPane.showMessageDialog(null, "Os valores das raízes reais são iguais à " + x1 + " e à " + x2);
         }
     }
 }
-
