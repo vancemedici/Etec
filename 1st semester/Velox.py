136e611522e358758d963d61a8031997f155d183
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-08 23:50:23 -0300

    Update Velox.py

--- 1st semester/Velox.py
+++ 1st semester/Velox.py
@@ -2,5 +2,6 @@ model = input('Qual carro foi alugado? ')
 daily_price = float(input('Quanto custa a diária desse modelo? '))
 days = int(input('Alugaram-o por quantos dias? '))
 km = float(input('Foram percorridos quantos quilômetros? '))
+
 print(f'O modelo de carro alugado foi {model}')
 print(f'As despesas totalizam R$ {(days * daily_price + km * 0.79):.2f}')
