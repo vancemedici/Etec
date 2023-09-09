7d9201eff3f67b2204a23f4f7b8102cf6b64bdc5
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-08 21:43:23 -0300

    Update NewInfo.java

--- 1st semester/Agenda 05/NewInfo.java
+++ 1st semester/Agenda 05/NewInfo.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package newinfo;
 
 import javax.swing.JOptionPane;
-
-/**
- *
- * @author Brian
- */
 public class NewInfo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int age;
         age = Integer.parseInt(JOptionPane.showInputDialog("Quantos anos você tem?"));
