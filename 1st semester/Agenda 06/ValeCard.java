00a9b469f072a417e2fe3b85cc42e85e931dfa13
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-08 22:52:46 -0300

    Update ValeCard.java

--- 1st semester/Agenda 06/ValeCard.java
+++ 1st semester/Agenda 06/ValeCard.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package valecard;
 
 import javax.swing.JOptionPane;
-
-/**
- *
- * @author Brian
- */
 public class ValeCard {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int license_plate;
         do {
