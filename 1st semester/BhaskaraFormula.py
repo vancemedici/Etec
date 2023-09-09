de2fc8bb4ad4b37e54a3772bdc673fbaf8a71dd6
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-09 00:19:54 -0300

    Update BhaskaraFormula.py

--- 1st semester/BhaskaraFormula.py
+++ 1st semester/BhaskaraFormula.py
@@ -2,14 +2,16 @@ import math
 a = float(input("Insira o valor de a "))
 b = float(input("Insira o valor de b "))
 c = float(input("Insira o valor de c "))
-delta = math.pow(b, 2) - 4 * a * c
-print("O valor de delta é "+str(delta))
+
+DELTA = math.pow(b, 2) - 4 * a * c
+print(f"O valor de delta é {delta:.3f}")
+
 if delta < 0:
     print("Não existem raízes reais")
 elif delta == 0:
-    x = -b / 2*a
-    print("Só há uma raíz real, cujo valor é "+str(x))
+    x = -b / (2*a)
+    print(f"Só há uma raiz real, cujo valor é {x:.3f}")
 else:
-    x1 = (-b + math.sqrt(delta)) / 2*a
-    x2 = (-b - math.sqrt(delta)) / 2*a
-    print("Os valores das raízes reais são iguais à "+str(x1)+" e à "+str(x2))
+    x1 = (-b + math.sqrt(delta)) / (2*a)
+    x2 = (-b - math.sqrt(delta)) / (2*a)
+    print(f"Os valores das raízes reais são iguais à {x1:.3f} e à {x2:.3f}")
