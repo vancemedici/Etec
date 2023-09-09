b794b6de214a5c41072b2416df057d9ed1026ca8
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-08 21:29:41 -0300

    Add files via upload

---
+++ 1st semester/Agenda 05/Codificando.py
@@ -0,0 +1,6 @@
+num1 = int(input("Insira o primeiro número "))
+num2 = int(input("Insira o segundo número "))
+if num1 == num2:
+    print("A soma desses números é igual à", num1 + num2)
+else:
+    print("O produto desses números é igual à", num1 * num2)
