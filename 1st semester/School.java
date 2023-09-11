34256cd25890ca3f68cd2a5a01d44b737c0c3fdb
Brian Fumagali <111618460+vancemedici@users.noreply.github.com>
2023-09-11 00:18:55 -0300

    Update School.java

--- 1st semester/School.java
+++ 1st semester/School.java
@@ -4,26 +4,26 @@ public class DisplayStudentData {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the student's name: ");
+        System.out.print("Digite o nome do estudante ");
         String name = input.nextLine();
 
-        System.out.print("Enter the student's grade: ");
+        System.out.print("Em que série ele está? ");
         String grade = input.nextLine();
 
-        System.out.print("Enter the student's phone number: ");
+        System.out.print("Qual é o telefone desse estudante? ");
         String phoneNumber = input.nextLine();
 
-        System.out.print("Enter the student's average: ");
+        System.out.print("Qual é a média desse estudante? ");
         double average = input.nextDouble();
 
         input.nextLine();
 
         System.out.println("--------------------Técnico em Informática Ead--------------------");
-        System.out.println("Name: " + name);
-        System.out.println("Phone Number: " + phoneNumber);
-        System.out.println("Grade: " + grade);
+        System.out.println("Nome: " + name);
+        System.out.println("Teoefone " + phoneNumber);
+        System.out.println("Série " + grade);
         System.out.println("-------------------------------------------------------------------------");
-        System.out.println("Average: " + average);
+        System.out.println("Média " + average);
         System.out.println("-------------------------------------------------------------------------");
 
         input.close();
